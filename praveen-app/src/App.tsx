5037ccc9159f4e753e5992a07c045670164d9248
Saipraveen1919 <saipraveen749@gmail.com>
2024-12-20 09:14:55 +0530

    first commit

---
+++ praveen-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import Home from './Components/Home.'
+import Movies from './Components/movie'
+
+
+
+export default function App() {
+  return (
+    <div>
+     <Movies/>
+     
+      </div>
+  )
+}
+
---
+++ praveen-app/src/Components/char_a = 'A'.py
@@ -0,0 +1,3 @@
+char_b = 'A'
+ascii_b = ord(char_b)
+print(ascii_b)
